62ee3a1afd2e3b5d4a8ecd34397a867c985236d6
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-06 14:36:27 +0530

    initial commit

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,14 @@
 // frontend/pages/index.js
 
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "2rem" }}>
       <h1>Welcome to the Task Management System</h1>
-      <p>
-        Use the navigation to login, register, or access your dashboard.
-      </p>
+      <Link href="/register" passHref>
+        <button>Go to Register</button>
+      </Link>
     </div>
   );
 }
