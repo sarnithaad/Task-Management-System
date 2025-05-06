c44e166a19dd960d800a9b9a548219e2209c925f
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-06 19:53:51 +0530

    initial commit

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,15 +1,14 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 
-module.exports = async (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  if (!authHeader) return res.status(401).json({ message: 'No token provided' });
-  const token = authHeader.split(' ')[1];
+module.exports = function(req, res, next) {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id);
+    req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Invalid token' });
+    res.status(401).json({ msg: 'Token is not valid' });
   }
 };
