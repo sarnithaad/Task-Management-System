e69252aee5ff414af45f11b5bc62924bd0f54029
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-06 19:58:44 +0530

    initial commit

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,18 +1,15 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: typeof window === "undefined" ? process.env.API_URL : '', // or your API base
-  headers: {
-    'Content-Type': 'application/json',
-  },
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
 });
 
-instance.interceptors.request.use(config => {
-  if (typeof window !== "undefined") {
+api.interceptors.request.use((config) => {
+  if (typeof window !== 'undefined') {
     const token = localStorage.getItem('token');
     if (token) config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
 
-export default instance;
+export default api;
