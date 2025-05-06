e90b7ea8113916295936fcdcf76464ee2d426a59
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-06 19:52:19 +0530

    initial commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,8 +4,14 @@ const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  dob: Date,
-  doj: Date,
+});
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
+UserSchema.methods.comparePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
 module.exports = mongoose.model('User', UserSchema);
