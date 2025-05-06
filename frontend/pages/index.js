7d91aae580733afccfc7b5ce4c3d8ce8f9846cee
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-06 14:44:32 +0530

    initial commit

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,18 +1,18 @@
-// frontend/pages/index.js
-
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleRegisterClick = () => {
-    router.push('/register');
-  };
-
   return (
     <div style={{ textAlign: "center", marginTop: "2rem" }}>
       <h1>Welcome to the Task Management System</h1>
-      <button onClick={handleRegisterClick}>Go to Register</button>
+      <p>Use the navigation to login, register, or access your dashboard.</p>
+      <div style={{ marginTop: "2rem" }}>
+        <Link href="/register">
+          <button style={{ marginRight: "1rem" }}>Go to Register</button>
+        </Link>
+        <Link href="/login">
+          <button>Go to Login</button>
+        </Link>
+      </div>
     </div>
   );
 }
