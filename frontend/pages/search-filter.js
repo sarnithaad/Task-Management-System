e03453554c67a4029b9741dfca1682e81f87244b
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-06 20:04:14 +0530

    initial commit

--- frontend/pages/search-filter.js
+++ frontend/pages/search-filter.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+import api from '../utils/api';
 import TaskFilters from '../components/TaskFilters';
+import TaskList from '../components/TaskList';
 
-export default function SearchFilterPage() {
+export default function SearchFilter() {
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async (params) => {
+    const res = await api.get('/tasks/search', { params });
+    setResults(res.data);
+  };
+
   return (
-    <div style={{ fontFamily: "Arial, sans-serif", padding: "20px" }}>
-      <h2 style={{ textAlign: "center", marginTop: "2rem" }}>Search & Filter Tasks</h2>
-      <TaskFilters showResults />
+    <div style={{ maxWidth: 600, margin: '50px auto' }}>
+      <h2>Search / Filter Tasks</h2>
+      <TaskFilters onSearch={handleSearch} />
+      <TaskList tasks={results} title="Results" />
     </div>
   );
 }
