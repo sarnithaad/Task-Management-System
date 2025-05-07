386f4470e1df9334e6673b66c8bee1b70e0964a3
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-07 16:52:04 +0530

    initial commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 UserSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
