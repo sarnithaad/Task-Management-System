752fa6a2195ec34d99b43e40fc61918cd96f47f5
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-07 16:49:36 +0530

    initial commit

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api',
+  withCredentials: false,
 });
 
 api.interceptors.request.use((config) => {
