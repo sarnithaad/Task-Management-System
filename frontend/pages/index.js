fdd81cf9c5d0dd7b7f5a6e5670f87945d35a7be9
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-07 16:37:13 +0530

    initial commit

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   const router = useRouter();
   return (
     <div style={{ textAlign: 'center', marginTop: 100 }}>
-      <h1>Welcome User</h1>
+      <h1>Task Manager </h1>
       <button onClick={() => router.push('/register')}>Register</button>
       <button onClick={() => router.push('/login')}>Login</button>
     </div>
