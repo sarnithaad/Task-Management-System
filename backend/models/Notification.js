ad46b16e8fdf3086aabd62e7be5289b4ed79a70b
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-09 16:18:43 +0530

    Update Notification.js

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -4,6 +4,7 @@ const NotificationSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   message: String,
   read: { type: Boolean, default: false },
+  task: { type: mongoose.Schema.Types.ObjectId, ref: 'Task' }, // <-- Add this line
   createdAt: { type: Date, default: Date.now },
 });
 
